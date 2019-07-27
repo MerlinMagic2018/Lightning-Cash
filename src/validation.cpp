20209f84063fe36f2d41c2657532be7bdca27ec4
Lightningcashgold-dev <lightningcashgold@gmail.com>
2019-07-27 15:12:55 -0400

    Mainet Test 1

--- src/validation.cpp
+++ src/validation.cpp
@@ -221,8 +221,8 @@ size_t nCoinCacheUsage = 5000 * 300;
 uint64_t nPruneTarget = 0;
 int64_t nMaxTipAge = DEFAULT_MAX_TIP_AGE;
 bool fEnableReplacement = DEFAULT_ENABLE_REPLACEMENT;
-const int nYesPowerFork = 150; // 247777
-const int nSpeedFork = 165; // ????
+const int nYesPowerFork = 247777; // 247777
+const int nSpeedFork = 298720; // ????
 
 uint256 hashAssumeValid;
 arith_uint256 nMinimumChainWork;
